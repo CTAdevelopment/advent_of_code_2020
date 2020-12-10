88c35b36b7aa8042604f4420a60038df93c6cd78
koit_the_artist <34422138+CTAdevelopment@users.noreply.github.com>
2020-12-10 22:46:47 +0100

    samll update f. name

--- day10.py
+++ day10.py
@@ -1,7 +1,7 @@
 data, used_voltages = [int(i) for i in open('day10.txt', 'r').read().splitlines()], [1, 3]
 data.sort()
 
-def determine_voltage_clean(data, used_voltages):
+def determine_voltage(data, used_voltages):
     next_voltage = 0
     for input_voltage in data:
         next_voltage += 1
@@ -14,4 +14,4 @@ def determine_voltage_clean(data, used_voltages):
                 print(int(used_voltages.count(1)) * int(used_voltages.count(3)))
                 return
 
-determine_voltage_clean(data, used_voltages)
+determine_voltage(data, used_voltages)
