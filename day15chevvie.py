0f23a17b2816bfb652d6c1d32f981ab193dd1959
koit_the_artist <34422138+CTAdevelopment@users.noreply.github.com>
2020-12-21 01:11:48 +0100

    day 15 final day 16.1 final

--- day15chevvie.py
+++ day15chevvie.py
@@ -1,52 +1,13 @@
+inputString = [13,16,0,12,15,1]
 
-import time
-now = time.time()
+def playMemory(target):
+    memory = {i : inputString.index(i) for i in inputString[:-1]}
+    lastNumber = inputString[-1]
+    for turn in range(len(inputString)-1, target-1):
+        currNumber = turn - memory[lastNumber] if lastNumber in memory.keys() else 0
+        memory[lastNumber] = turn
+        lastNumber = currNumber
+    print(lastNumber)
 
-def one():
-    start_data = [13,16,0,12,15,1]
-    x = 30000000 - len(start_data)
-
-    while x != 0:
-
-        iets = start_data[-1]
-        iteration_pos = len(start_data) - 1
-
-        for iets_anders in reversed(start_data[0:len(start_data)-1]):
-            if iets == iets_anders:
-                nieuwe_getal = len(start_data) - iteration_pos
-                start_data.append(nieuwe_getal)
-                break
-
-            iteration_pos -= 1
-
-            if iteration_pos == 0:
-                start_data.append(0)
-
-        x -= 1
-
-    print(start_data[-1])
-    print(time.time() - now)
-
-def two():
-    start_data = [13,16,0,12,15,1]
-    x = (2020 - len(start_data))
-
-    while x != 0:
-
-        print(start_data)
-        a = start_data[-1]
-        z = start_data[0:len(start_data) - 1]
-
-        if a in z:
-            new = len(start_data) - start_data[0: len(start_data) - 1].index(a) -1
-            start_data.append(new)
-        else:
-            start_data.append(0)
-
-        x -= 1
-
-    print(start_data[-1])
-
-
-two()
-print('tijd', now-time.time())
+playMemory(2020)
+playMemory(30000000)
--- day16chevvie.py
+++ day16chevvie.py
@@ -3,17 +3,25 @@ lines = open('tickets.txt', 'r').read().splitlines()
 sum=0
 
 kaassouffle = {
-    i.split(':')[0] : [ range(int(i.split(':')[1].split(' or ')[0].split('-')[0]), int(i.split(':')[1].split(' or ')[0].split('-')[1]) + 0 ),
-                        range(int(i.split(':')[1].split(' or ')[1].split('-')[0]), int(i.split(':')[1].split(' or ')[1].split('-')[1]) + 0 ) ]
+    i.split(':')[0] : [ range(int(i.split(':')[1].split(' or ')[0].split('-')[0]), int(i.split(':')[1].split(' or ')[0].split('-')[1]) + 1 ),
+                        range(int(i.split(':')[1].split(' or ')[1].split('-')[0]), int(i.split(':')[1].split(' or ')[1].split('-')[1]) + 1 ) ]
                         for i in params
             }
 
+print(kaassouffle)
 
 for line in lines:
     for ticket in [int(i) for i in line.split(',')]:
+        max_ranges = len(kaassouffle.values())
+        boo = False
         for ranges in kaassouffle.values():
-            for range in ranges:
-                if ticket not in range and range == ranges[-1]:
-                    sum += ticket
+            for r in ranges:
+                if ticket in r:
+                    boo = True
+                    break
+
+            max_ranges -= 1
+            if max_ranges == 0 and not boo:
+                sum += ticket
 
 print(sum)
