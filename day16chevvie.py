96bfc4044577574fdd3bb34cbec425f029597544
koit_the_artist <34422138+CTAdevelopment@users.noreply.github.com>
2020-12-23 00:34:48 +0100

    mehh

--- day16chevvie.py
+++ day16chevvie.py
@@ -38,19 +38,50 @@ print(len(valid_ids))
 
 def define_order_params(valid_ids_1):
 
-    kroket = {i : 0 for i in kaassouffle.keys()}
+    kroket = {i : [0, 0] for i in kaassouffle.keys()}
+    found_ticket_locations = []
+
+    for ticket in [int(i) for i in valid_ids_1.split(',')]:
+        ticket_ind = valid_ids_1.index(str(ticket))
+
+        for id, values in kaassouffle.items():
+            if ticket in values[0] or ticket in values[1]:
+                if kroket[id][0] == ticket_ind:
+                    continue
+                else:
+                    kroket[id] = ticket_ind, ticket
+                    found_ticket_locations.append(ticket)
+                    print(ticket)
+
+        if ticket == valid_ids_1[-1]:
+            temp_list = []
+
+            while len(found_ticket_locations) != len(valid_ids_1.split(',')):
+                for id in [int(i) for i in valid_ids_1.split(',')]:
+                    id_index = valid_ids_1.index(str(id))
+                    if id not in found_ticket_locations:
+                        print('id not found in kroket:',id)
+                        for idx, valuesx in kaassouffle.items():
+                            if id in valuesx[0] or id in valuesx[1]:
+                                found_ticket_locations.append(id)
+                                temp_list.append(kroket[idx])
+                                found_ticket_locations.remove(kroket[idx][1])
+                                kroket[idx] = id_index, id
+                                continue
+
+                if len(temp_list) > 0:
+                    for i in temp_list:
+                        ticket_ind = i[0]
+                        ticket_ = i[1]
+                        for idx, valuesx in kaassouffle.items():
+                            if ticket_ in valuesx[0] or ticket_ in valuesx[1] and kroket[idx][0] == 0:
+                                kroket[idx] = ticket_ind, ticket_
+                                continue
+
     print(kroket)
 
-    for parameter in [int(i) for i in valid_ids_1.split(',')]:
-
-        for idx, ranges in kaassouffle.items():
-            if parameter in ranges[0] or parameter in ranges[1]:
-                if kroket[idx] == 0 and valid_ids_1.index(str(parameter)) not in kroket.values():
-                    kroket[idx] = valid_ids_1.index(str(parameter))
 
 
-
-    print(kroket)
 
 
 define_order_params(valid_ids[0])
